1d95fb4c7865da0f16d47b54ed31775e3a6bf12a
c6tr4by795-byte <c6tr4by795@privaterelay.appleid.com>
2026-07-01 18:00:52 +0300

    Delete auth.js

--- auth.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("button").addEventListener("click", function (e) {
-
-    e.preventDefault();
-
-    alert("تم تسجيل الدخول");
-
-    window.location.href = "scanner.html";
-
-});
