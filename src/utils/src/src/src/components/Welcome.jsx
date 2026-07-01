dfa3f2da54852654d7ccd64dfd07841ccf1b02b7
c6tr4by795-byte <c6tr4by795@privaterelay.appleid.com>
2026-07-01 18:24:36 +0300

    Update Welcome.jsx

--- src/utils/src/src/src/components/Welcome.jsx
+++ src/utils/src/src/src/components/Welcome.jsx
@@ -1,22 +1,21 @@
 import { useState } from "react";
 import { checkPin } from "../utils/pin";
 import PinInput from "./PinInput";
+import PrimaryButton from "./PrimaryButton";
 
 export default function Welcome({ onSuccess }) {
   const [pin, setPin] = useState("");
 
   const handleChange = (e) => {
-    const value = e.target.value.replace(/\D/g, "").slice(0, 6);
+    setPin(e.target.value.replace(/\D/g, "").slice(0, 6));
+  };
 
-    setPin(value);
-
-    if (value.length === 6) {
-      if (checkPin(value)) {
-        onSuccess();
-      } else {
-        alert("الرمز السري غير صحيح");
-        setPin("");
-      }
+  const handleLogin = () => {
+    if (checkPin(pin)) {
+      onSuccess();
+    } else {
+      alert("الرمز السري غير صحيح");
+      setPin("");
     }
   };
 
@@ -30,6 +29,10 @@ export default function Welcome({ onSuccess }) {
         value={pin}
         onChange={handleChange}
       />
+
+      <PrimaryButton onClick={handleLogin}>
+        التالي
+      </PrimaryButton>
     </div>
   );
 }
