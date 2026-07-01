11bcc95813c1f51aa79f09497f3c29d77287ad44
c6tr4by795-byte <c6tr4by795@privaterelay.appleid.com>
2026-07-01 20:29:32 +0300

    Add Home component with centered layout

---
+++ Home.jsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "35px",
+        fontWeight: "bold",
+      }}
+    >
+      الصفحة الرئيسية
+    </div>
+  );
+}
