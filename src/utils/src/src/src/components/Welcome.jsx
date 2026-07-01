db22ac76866e71601f04c32e379bccb2d68f320b
c6tr4by795-byte <c6tr4by795@privaterelay.appleid.com>
2026-07-01 18:17:38 +0300

    Update Welcome.jsx

--- src/utils/src/src/src/components/Welcome.jsx
+++ src/utils/src/src/src/components/Welcome.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
 import { checkPin } from "../utils/pin";
+import PinInput from "./PinInput";
 
 export default function Welcome({ onSuccess }) {
   const [pin, setPin] = useState("");
 
   const handleChange = (e) => {
     const value = e.target.value.replace(/\D/g, "").slice(0, 6);
+
     setPin(value);
 
-    if (value.length === 6 && checkPin(value)) {
-      onSuccess();
+    if (value.length === 6) {
+      if (checkPin(value)) {
+        onSuccess();
+      } else {
+        alert("الرمز السري غير صحيح");
+        setPin("");
+      }
     }
   };
 
@@ -17,15 +24,11 @@ export default function Welcome({ onSuccess }) {
     <div className="welcome">
       <h1>مرحباً 👋</h1>
 
-      <p>أدخل الرمز السري</p>
+      <p>أدخل الرمز السري للمتابعة</p>
 
-      <input
-        type="password"
-        inputMode="numeric"
-        maxLength={6}
+      <PinInput
         value={pin}
         onChange={handleChange}
-        placeholder="******"
       />
     </div>
   );
